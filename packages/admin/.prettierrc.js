8fc3f005efcca5feadbd2bb9c61864291be6cabd
Turnbulll <71480607+Turnbulll@users.noreply.github.com>
2025-04-03 20:18:10 +1100

    rm unused code

--- packages/admin/.prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  semi: true,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'all',
-  arrowParens: 'always',
-  printWidth: 80,
-};
--- shared/types/admin.d.ts
+++
@@ -1,4 +0,0 @@
-export interface Admin {
-  userSub: string;
-  userEmail: string;
-}
